d805c0ed0d607d12218c2825951acfd35a1fb71d
Myungjae Kwak <myungjae.kwak@mga.edu>
2024-07-11 19:25:04 -0400

    Add BookMvc project

---
+++ apps/BookMvc/BookMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my hello world default page.";
+        //}
+
+        public string Welcome()
+        {
+            return "This is my welcome page.";
+        }
+    }
+}
---
+++ apps/BookMvc/BookMvc/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookMvc.Models;
+
+namespace BookMvc.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
