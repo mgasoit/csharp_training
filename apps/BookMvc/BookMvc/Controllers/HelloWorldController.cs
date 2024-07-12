8a018a9921e9c8c14eec0e96b174c7e8e4d9a811
Myungjae Kwak <myungjae.kwak@mga.edu>
2024-07-11 21:07:05 -0400

    Modify HelloWorldController

--- apps/BookMvc/BookMvc/Controllers/HelloWorldController.cs
+++ apps/BookMvc/BookMvc/Controllers/HelloWorldController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BookMvc.Models;
 
 namespace BookMvc.Controllers
 {
@@ -18,5 +19,33 @@ namespace BookMvc.Controllers
         {
             return "This is my welcome page.";
         }
+
+        public IActionResult Books()
+        {
+
+            IList<Book> bookList = new List<Book>();
+            bookList.Add(new Book() { Id = 1, Author = "John", Title="C# ASP.NET Core", ISBN="9831234567890" });
+            bookList.Add(new Book() { Id = 2, Author = "Doe", Title = "C# Windows Forms", ISBN = "9831234345891" });
+            bookList.Add(new Book() { Id = 3, Author = "Mike", Title = "C# Console App", ISBN = "9830988765678" });
+            ViewData["books"] = bookList;
+
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBook(Book book)
+        {
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+            Console.WriteLine(book.ISBN);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
